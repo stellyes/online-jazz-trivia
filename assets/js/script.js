ebd28ba4f855a77d3b5c779b1316d91d065211ea
Ryan England <me@rengland.org>
2023-09-30 02:28:32 -0700

    Added most navigation logic. Just needs a closing screen and local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,67 @@
+// Handles import for questions as a JSON object
 import questions from "./questions.json" assert { type: "json" };
-
-console.log(questions);
 
 var startMenu = document.querySelector(".start-menu");
 var startButton = document.querySelector("#start-button");
 var leaderboardButton = document.querySelector("#leaderboard-button");
+
 var questionView = document.querySelector(".question");
-var quitButton = document.querySelector("#quit-game");
-var endGameView = document.querySelector("#end-game");
+var questionTitle = document.querySelector(".question-title");
+var questionAnswers = document.querySelector(".answers");
 var timerEl = document.querySelector("#time");
+var timeInterval; // Global time variable to prevent overlapping conflict
+var answerReply = document.querySelector("#answer-reply");
+var scoreEl = document.querySelector("#score");
+var submitButton = document.querySelector("#submit-answer");
+var quitButton = document.querySelector("#quit-game");
 
-var secondsRemaining = 60;
+var endGameView = document.querySelector("#end-game");
 
-// Hides main menu and displays questions
-startButton.addEventListener("click", function () {
-  startMenu.setAttribute("style", "display: none");
-  questionView.setAttribute("style", "display: flex");
-  countdown();
-});
+// List of questions, index of current question,
+// and selected answer object
+var questionList = questions.list;
+var currentQuestion = 0;
+var selectedAnswer = { default: "value" };
+var secondsRemaining = 540;
+var currentScore = 0;
 
-// If user decides to quit mid-game, this returns them to
-quitButton.addEventListener("click", function () {
-  startMenu.removeAttribute("style", "display: none");
-  questionView.removeAttribute("style", "display: flex");
-});
+var correctAnswer = "Correct! ✅";
+var incorrectAnswer = "Incorrect! ❌";
+
+// Implementation of the Fisher-Yates shuffle
+// To randomize order of questions each game start
+function fisherYates(array) {
+  for (var i = array.length - 1; i > 0; i--) {
+    var j = i;
+
+    // Ensures no wasted moves in the instance
+    // that the same index as i is chosen
+    while (j === i) {
+      j = Math.floor(Math.random() * i);
+    }
+
+    // Make swap
+    var palceholder = array[i];
+    array[i] = array[j];
+    array[j] = palceholder;
+  }
+
+  return array;
+}
+
+// Purely decorational. Fades elements in and out.
+function fadeElement(element) {
+  var opacity = 1; // initial opacity
+  var transitionTimer = setInterval(function () {
+    if (opacity <= 0.05) {
+      clearInterval(transitionTimer);
+      element.textContent = "";
+    }
+    element.style.opacity = opacity;
+    element.style.filter = `alpha(opacity= ${opacity} * 100)`;
+    opacity -= opacity * 0.05;
+  }, 20);
+}
 
 // Needs work
 function endScreen() {
@@ -34,14 +72,9 @@ function endScreen() {
 function countdown() {
   // Prevents delay in load time,
   timerEl.textContent = "#:##";
-
-  // Resets value if game was previously run
   secondsRemaining = 60;
 
-  // Gets local variable as to not directly reference
-  // and modify global value
-
-  var timeInterval = setInterval(function () {
+  timeInterval = setInterval(function () {
     // If 60 seconds, should display "1:00" and then "0:59" sequentially to 0
     //
     //  [Integer division of 60 to get minutes]
@@ -68,6 +101,80 @@ function countdown() {
   }, 1000);
 }
 
-function fillQuestion() {}
+// Gets answer information and time remaining
+// to calculate how many points the user
+// earned. Only called by submitButton event listener,
+// abstracted for readability.
+function updateScore() {
+  // Signifies default value, prints 0's to scoreEl
+  if (selectedAnswer === `{ default: "value" }`) {
+    scoreEl.textContent = ("00000" + currentScore).slice(-5);
+  }
 
-function questionLogic() {}
+  // User earns points with remaining time / 5 + 250 for right answer
+  // as well as 15 additional seconds to their quiz time
+  if (selectedAnswer["correct"] === true) {
+    currentScore += Math.floor(secondsRemaining / 5) + 250;
+    scoreEl.textContent = ("00000" + currentScore).slice(-5);
+    secondsRemaining += 15;
+  }
+}
+
+// Takes in index of question child and occupies text
+// fields with corresponding values
+function fillQuestion() {
+  questionTitle.textContent = questionList[currentQuestion].questionPrompt;
+
+  // Shuffle answers in random order for
+  // each occurance and fills text conent
+  var answers = fisherYates(questionList[currentQuestion].answers);
+  for (var i = 0; i < 4; i++) {
+    questionAnswers.children[i].textContent = answers[i].text;
+  }
+}
+
+// Hides main menu and displays questions
+startButton.addEventListener("click", function () {
+  currentScore = 0; // Reset score if previous game was played
+  scoreEl.textContent = "00000"; // Reset score display
+  questionList = fisherYates(questionList);
+  fillQuestion();
+  startMenu.setAttribute("style", "display: none");
+  questionView.setAttribute("style", "display: flex");
+  countdown();
+});
+
+// Monitors user activity for selected answer and stores
+// corresponding selection's data in selectedAnswer
+questionAnswers.addEventListener("click", function () {
+  var answerIndex = document.activeElement.getAttribute("tabindex") - 1;
+  selectedAnswer = questionList[currentQuestion].answers[answerIndex];
+});
+
+// Submits question and progresses to next question
+submitButton.addEventListener("click", function () {
+  // If question index is equal to last index in
+  // questionList, close game.
+  if (currentQuestion === questionList.length - 1) {
+    endScreen();
+  }
+
+  if (selectedAnswer["correct"] === true) {
+    answerReply.textContent = correctAnswer;
+  } else {
+    answerReply.textContent = incorrectAnswer;
+  }
+  fadeElement(answerReply);
+
+  updateScore(); // Update user score
+  currentQuestion++; // Increment to next question in list
+  fillQuestion(); // Occupy text fields with new question
+});
+
+// If user decides to quit mid-game, this returns them to
+// home screen. Timer is immediately terminated.
+quitButton.addEventListener("click", function () {
+  clearInterval(timeInterval);
+  startMenu.removeAttribute("style", "display: none");
+  questionView.removeAttribute("style", "display: flex");
+});
